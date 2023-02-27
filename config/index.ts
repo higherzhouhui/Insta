a743086e772e98f3579daeac4ae374671e3cd3be
zhouhui <782492184@qq.com>
2023-02-27 18:02:27 +0800

    fix bug

--- config/index.ts
+++ config/index.ts
@@ -5,12 +5,12 @@ let apiUrl = 'http://pd1.test.chatnowmeta.com';
 
 if (process.env.APP_ENV === 'development') {
   webUrl = 'http://localhost:10002';
-  apiUrl = 'http://test.web.pd-1st.com';
+  apiUrl = 'https://www.pd-1st.com';
 }
 
 if (process.env.APP_ENV === 'test') {
   webUrl = 'https://www.pd-1st.com';
-  apiUrl = 'http://test.web.pd-1st.com';
+  apiUrl = 'https://www.pd-1st.com';
 }
 
 if (process.env.APP_ENV === 'production') {

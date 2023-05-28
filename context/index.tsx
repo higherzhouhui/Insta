7c0ee06e0fcc20f7151b8cbaf242ea930325b175
higherzhouhui <782492184@qq.com>
2023-05-28 14:35:54 +0800

    remove langprovider

--- context/index.tsx
+++ context/index.tsx
@@ -3,8 +3,6 @@ import {SkeletonTheme} from 'react-loading-skeleton';
 import {RecoilRoot} from 'recoil';
 import {ThemeProvider} from 'styled-components';
 
-import {LanguageProvider} from './LanguageProvider';
-
 import {Web3ContextProvider} from '@/ethers-react';
 import {lightTheme} from '@/uikit';
 
@@ -13,15 +11,13 @@ type ProvidersProps = {};
 const Providers: React.FC<ProvidersProps> = ({children}) => {
   return (
     <ThemeProvider theme={lightTheme}>
-      <LanguageProvider>
-        <RecoilRoot>
-          <Web3ContextProvider>
-            <SkeletonTheme baseColor='#ebf0f6' highlightColor='#ffffff'>
-              {children}
-            </SkeletonTheme>
-          </Web3ContextProvider>
-        </RecoilRoot>
-      </LanguageProvider>
+      <RecoilRoot>
+        <Web3ContextProvider>
+          <SkeletonTheme baseColor='#ebf0f6' highlightColor='#ffffff'>
+            {children}
+          </SkeletonTheme>
+        </Web3ContextProvider>
+      </RecoilRoot>
     </ThemeProvider>
   );
 };

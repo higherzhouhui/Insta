da2d8fbdd53121a970e89225a376aa55a1a0c845
higherzhouhui <782492184@qq.com>
2023-07-19 02:15:44 +0800

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,33 @@ import Document, {
   DocumentContext,
 } from 'next/document';
 import Script from 'next/script';
+import {ServerStyleSheet} from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {...initialProps};
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
@@ -26,7 +48,7 @@ class MyDocument extends Document {
             name='keywords'
           />
           <meta
-            content='width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
+            content='width=device-width, initial-scale=1.0'
             name='viewport'
           />
           <link href='/favicon.ico' rel='icon' />

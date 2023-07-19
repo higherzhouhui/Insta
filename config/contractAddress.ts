4c6b88b794927afb859fc060606f3474c57f61ad
higherzhouhui <782492184@qq.com>
2023-07-19 22:14:01 +0800

    update

--- config/contractAddress.ts
+++ config/contractAddress.ts
@@ -1,5 +1,5 @@
 export const USDTADDRESS = '0x55d398326f99059fF775485246999027B3197955';
 export const WBNBADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-export const USECHAINID = 97;
+export const USECHAINID = 56;
 export const RPCPROVIDERURL =
   'https://fittest-yolo-market.bsc-testnet.discover.quiknode.pro/461d5212d991d510478e2e1ecf53441b8539fe3a/';
